b0acbf013e0701a09ed69debec744a4114726a5a
Hector Gonzalez <gorozco.hector@gmail.com>
2022-07-11 18:27:42 -0400

    Fix number overflow on display

--- scripts/script.js
+++ scripts/script.js
@@ -34,7 +34,10 @@ const factorial = function(num) {
 //function that takes two numbers and an operator
 //and calls the relevant function from above
 // String Int Int -> Int
-function operate (operator, num1, num2){
+function operate (operator, stringNum1, stringNum2){
+    //convert to integers
+    let num1 = parseInt(stringNum1);
+    let num2 = parseInt(stringNum2)
     switch (operator) {
         case '+':
             return add(num1, num2);
@@ -48,3 +51,73 @@ function operate (operator, num1, num2){
             break   
     }
 }
+
+
+const numberButtons = document.querySelectorAll('.number-button')
+const operatorButtons = document.querySelectorAll('.operator-button')
+const display = document.querySelector('#display')
+const equalsButton = document.querySelector('#equals-button')
+let displayValue = ''
+let previousNumber = 0
+let currentOperator = ''
+let operatorJustPressed = false
+let equalsJustP
+
+//updates display when clicking on number buttons
+function populateDisplay (number) {
+    if (displayValue.length <= 10) {//limit the number of digits to 10
+    if(operatorJustPressed){//if an operator is pressed clear the display
+        displayValue = number
+        operatorJustPressed = false;
+    } else {
+    displayValue += number
+    }
+    display.innerText = displayValue
+    }
+};
+
+//adds previous function to number buttons
+numberButtons.forEach(button=> {
+    button.addEventListener('click', (e) => populateDisplay(e.target.innerText))
+});
+
+
+
+//store number when pressing an operator
+//and save which operator has been used
+operatorButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+
+        operatorJustPressed = true;
+
+        if (currentOperator != ''){//string together operations
+            let result = operate(currentOperator, previousNumber, displayValue)
+            displayResult(result)
+        }
+        previousNumber = displayValue;
+        currentOperator = e.target.innerText;
+    })
+});
+
+
+//use operate function with '=' button
+equalsButton.addEventListener('click', (e) => {
+    if (currentOperator != '') {//so that equals only works once
+    let result = operate(currentOperator, previousNumber, displayValue)
+    displayResult(result)
+    }
+})
+
+function displayResult(result) {
+    let numberLength = result.toString().length;
+    let numberDecimalLength = Math.floor(result).toString.length;
+    if (numberLength >= 10){//round long numbers
+        result = result.toFixed(10-numberDecimalLength-1);
+    };
+
+    displayValue = result;
+    display.innerText = result;
+    currentOperator = ''//restart operator to string together several operations
+    operatorJustPressed = true
+}
+
