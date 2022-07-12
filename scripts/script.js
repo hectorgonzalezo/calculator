04886d7dff6b3ce4c6b25366139c7f82f956b010
Hector Gonzalez <gorozco.hector@gmail.com>
2022-07-12 17:50:23 -0400

    Add "." button functionality

--- scripts/script.js
+++ scripts/script.js
@@ -35,9 +35,9 @@ const factorial = function (num) {
 //and calls the relevant function from above
 // String Int Int -> Int
 function operate(operator, stringNum1, stringNum2) {
-    //convert to integers
-    let num1 = parseInt(stringNum1);
-    let num2 = parseInt(stringNum2)
+    //convert to Floats
+    let num1 = parseFloat(stringNum1);
+    let num2 = parseFloat(stringNum2)
     switch (operator) {
         case '+':
             return add(num1, num2);
@@ -47,12 +47,12 @@ function operate(operator, stringNum1, stringNum2) {
             return multiply(num1, num2)
         case '/':
             //if user is trying to dive by 0
-            if (stringNum1 == '0' || stringNum2 == '0'){
+            if (stringNum1 == '0' || stringNum2 == '0') {
                 currentOperator = '';//so that equals doesn't try to display result
                 display.innerText = "Cant divide by zero!";
                 return 'divide by 0'
             } else {
-            return divide(num1, num2)
+                return divide(num1, num2)
             }
         default:
             break
@@ -65,29 +65,62 @@ const operatorButtons = document.querySelectorAll('.operator-button')
 const display = document.querySelector('#display')
 const equalsButton = document.querySelector('#equals-button')
 const clearButton = document.querySelector('#clear-button')
-let displayValue = ''
-let previousNumber = 0
-let currentOperator = ''
-let operatorJustPressed = false
-let equalsJustPressed = false
+const deleteButton = document.querySelector('#delete-button')
+const dotButton = document.querySelector('#dot-button')
 
-//updates display when clicking on number buttons
+//initializes values used by functions
+let displayValue = '';
+let previousNumber = 0;
+let currentOperator = '';
+let operatorJustPressed = false;
+let equalsJustPressed = false;
+
+const maximumDisplayLength = 15
+
+//updates display when clicking on number or "." buttons
 function populateDisplay(number) {
-    console.log(number)
     //limit the number of digits to 10, and stop adding zeroes at first
-    if (displayValue.toString().length <= 10 && !(number == '0' && displayValue == '')) {
+    if (displayValue.toString().length <= maximumDisplayLength && !(number == '0' && displayValue == '')) {
         if (operatorJustPressed) {//if an operator is pressed clear the display
             displayValue = number;
             operatorJustPressed = false;
-        } else if (equalsJustPressed){
-            equalsJustPressed = false;
+        } else if (equalsJustPressed) {
             displayValue = number;
-        } else  {
+            equalsJustPressed = false;
+        } else {
             displayValue += number
         }
         display.innerText = displayValue
+
     }
 };
+
+//Updates display when clicking on operator or "=" buttons
+function displayResult(result) {
+    //round long numbers, limit to 10 digits
+    let numberLength = result.toString().length;
+    let numberDecimalLength = Math.floor(result).toString.length;
+    if (numberLength >= maximumDisplayLength) {
+        result = result.toFixed(maximumDisplayLength - numberDecimalLength - 1);
+    };
+    //display result
+    displayValue = result;
+    display.innerText = result;
+    currentOperator = ''//restart operator to string together several operations
+    operatorJustPressed = true
+}
+
+//reinitialize values used by calculator functions
+function clearData() {
+    displayValue = '';
+    previousNumber = 0;
+    currentOperator = '';
+    operatorJustPressed = false;
+    equalsJustPressed = false;
+
+    displayResult('')
+}
+
 
 //adds previous function to number buttons
 numberButtons.forEach(button => {
@@ -118,39 +151,23 @@ equalsButton.addEventListener('click', (e) => {
     if (currentOperator != '') {//so that equals only works once
         let result = operate(currentOperator, previousNumber, displayValue);
         //if user didn't try to divide by 0
-        if (result != 'divide by 0'){
-        displayResult(result);
+        if (result != 'divide by 0') {
+            displayResult(result);
         };
         equalsJustPressed = true;
     }
 })
 
 
-//This function displays the result on the display area
-function displayResult(result) {
-    //round long numbers, limit to 10 digits
-    let numberLength = result.toString().length;
-    let numberDecimalLength = Math.floor(result).toString.length;
-    if (numberLength >= 10) {
-        result = result.toFixed(10 - numberDecimalLength - 1);
-    };
-    //display result
-    displayValue = result;
-    display.innerText = result;
-    currentOperator = ''//restart operator to string together several operations
-    operatorJustPressed = true
-}
-
 //wipe data with "clear" button
 clearButton.addEventListener('click', (e) => clearData())
 
-function clearData() {
-    let displayValue = ''
-    let previousNumber = 0
-    let currentOperator = ''
-    let operatorJustPressed = false
-    let equalsJustPressed = false
 
-    displayResult('')
-}
-
+//add a dot to result, but only if there no previous dot
+// 35.55 not 35.55.5.5
+dotButton.addEventListener('click', (e) => {
+    //if there's a button in display
+    if (!displayValue.includes('.')) {
+        populateDisplay(e.target.innerText)
+    }
+})
